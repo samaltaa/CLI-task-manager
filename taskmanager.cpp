c424f30ef9d8985c03d282eb70bce92178071a49
Samira <samirasanchez8090@gmail.com>
2023-08-29 17:48:09 -0400

    main function and choices defined

--- taskmanager.cpp
+++ taskmanager.cpp
@@ -19,3 +19,34 @@ private:
     std::string taskName;
     std::string taskDescription;
 };
+
+class TaskManager {
+public:
+    void addTask(const Task& task) {
+        tasks.push_back(task);
+    }
+
+    void listTasks() const {
+        std::cout << "Tasks: \n";
+        for (const Task& task : tasks) {
+            std::cout << "- " << task.getName() << ": " << task.getDescription() << "\n";
+        }
+    }
+
+private:
+    std::vector<Task> tasks;
+};
+
+int main() {
+    TaskManager taskManager;
+
+    while(true){
+        std::cout << "1. Add Task\n";
+        std::cout << "2. List Tasks\n";
+        std::cout << "3. Quit\n";
+        std::cout << "Choose an option: ";
+
+        int choice;
+        std::cin >> choice;
+    }
+}
